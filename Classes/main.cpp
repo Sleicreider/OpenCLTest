ef4a2077fee249d0c4305ec01607f3e469f085ce
FVW\dr <reisner@linkbit.io>
2019-01-14 13:14:36 +0100

    opencl initial test

--- Classes/main.cpp
+++ Classes/main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 
+#define CL_TARGET_OPENCL_VERSION 110
+#include <CL/cl.hpp>
+
+#include <vector>
+
 int main()
 {
 	std::cout << "Hello OpenCL" << std::endl;
+
+	std::vector<cl::Platform> platforms;
+	cl::Platform::get(&platforms);
+
+	if(platforms.empty())
+	{
+		std::cout << "No platforms found. Check OpenCL installation" << std::endl;
+	}
+    else
+    {
+        cl::Platform default_platform = platforms.front();
+		std::cout << "OpenCL platform: " << default_platform.getInfo<CL_PLATFORM_NAME>() << std::endl;
+	}
+
+
+	//prevent default exit
+	int i = 0;
+	std::cin >> i;
+
 	return 0;
 }
